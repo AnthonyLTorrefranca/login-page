c3f611fae865cdaf841912ae1ffb356a2ab8742f
Anthony <Anthony.Torrefranca@gmail.com>
2026-06-16 19:12:36 +0800

    initial commit

---
+++ login_page/src/components/Login/Login.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import './Login.css'
+
+export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [seePassword, setSeePassword] = useState(true)
+    function handle_submit(e){
+        e.preventDefault()
+        console.log(email, password)
+    }
+return (
+    <>
+    <div className="login_info">
+        <h1>Welcome Back!</h1>
+        <p>Login to continue your shopping journey.</p>
+
+        <form onSubmit={handle_submit}>
+
+            {/* email */}
+                <p>Email:</p>
+                <input onChange={(e) =>
+                    setEmail(e.target.value)}
+                    type="text" placeholder="Email or Username"/>
+
+            {/* password */}
+                <p>Password:</p>
+                <input onChange={(e) =>
+                    setPassword(e.target.value)}
+                    type={password ?  "text" :  "password"} 
+                    placeholder="Password"/>
+
+            <button className="seePswd" 
+                    onClick={() =>
+                        setSeePassword(false)}
+                    value={seePassword} 
+                    type="button"
+                        >See Password</button>
+
+            <button className="login_btn">Login</button>
+                <p>or</p>
+            <button type="button">Continue with Google</button>
+            <button type="button">Continue with Facebook</button>
+                <p>Don't have an account? <a href="#" target='_blank'>Sign up.</a></p>
+        </form>
+    </div>
+    </>
+)
+}
