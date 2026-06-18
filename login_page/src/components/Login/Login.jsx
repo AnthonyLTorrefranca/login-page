f36aeb906ec86afc8000603400b48ce3cd50055a
Anthony <Anthony.Torrefranca@gmail.com>
2026-06-18 16:47:46 +0800

    robust login page

--- login_page/src/components/Login/Login.jsx
+++ login_page/src/components/Login/Login.jsx
@@ -1,46 +1,56 @@
 import { useState } from "react";
 import './Login.css'
 
-export default function Login() {
-    const [seePswd, setSeePswd] = useState(false)
-    const [formValue, setFormValue] = useState({
+const Login = () => {
+    const [formValues, setFormValues] = useState({
         username: "",
         password: "",
     })
+    const [showPswd, setShowPswd] = useState(false)
     function handleSubmit(e){
         e.preventDefault()
-        console.log(formValue)
+        console.log(JSON.stringify(formValues))
+        setFormValues({
+        username: "",
+        password: "",
+    })
     }
 return (
     <section className="loginInfo">
         <h1>Welcome Back!</h1>
-            <p>Login to continue your shopping journey</p>
+        <p>Login to continue your shopping journey</p>
         <form onSubmit={handleSubmit}>
             <p>Enter username</p>
-            <section className="usernameContainer">
-                <input placeholder="Enter username"
-                    name="username" 
-                    type="text" 
-                    onChange={(e) => setFormValue(({ ...formValue, username: e.target.value }))}/>
-            </section>
-                <p>Enter password</p>
-            <section className="passwordContainer">
-                <input placeholder="Enter password"
-                    name="password" 
-                    type={seePswd ? "text" : "password"} 
-                    onChange={(e) => setFormValue(({ ...formValue, password: e.target.value }))}/>
-            <button 
-                type="button"
-                onClick={()=> setSeePswd(!seePswd)}>👁</button>
-            </section>
-                <button type="submit" className="loginBtn">Login</button>
+                <section className="usernameContainer">
+                    <input 
+                        type="text" 
+                        value={formValues.username}
+                        placeholder="Enter username"
+                        name="username" 
+                        onChange={(e) => setFormValues(prev =>({
+                            ...prev, username: e.target.value}))} />
+                </section>
+            <p>Enter password</p>
+                <section className="passwordContainer">
+                    <input 
+                        value={formValues.password}
+                        type={showPswd ? "text" : "password"} 
+                        placeholder="Enter password"
+                        name="password" 
+                        onChange={(e) => setFormValues(prev =>({
+                            ...prev, password: e.target.value}))} />
+                    <button type="button" onClick={()=> setShowPswd(!showPswd)}>👁</button>
+                </section>
+                    <button className="loginBtn">Login</button>
         </form>
             <p>or</p>
-        <section>
+        <section className="social">
             <button className="continueWithSocials">Continue with Google</button>
             <button className="continueWithSocials">Continue with Facebook</button>
         </section>
-        <p>Don't have an account? <a href="#">Sign up.</a></p>
+            <p>Don't have an account? <a href="#">Sign up.</a></p>
     </section>
-    )
+)
 }
+
+export default Login
