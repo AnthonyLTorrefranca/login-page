7009ee05526e6f047de3d5b8a29dc2e226ef2113
Anthony <Anthony.Torrefranca@gmail.com>
2026-06-21 14:03:19 +0800

    created a mock user data

--- login_page/src/components/Login/Login.jsx
+++ login_page/src/components/Login/Login.jsx
@@ -9,12 +9,25 @@ export default function Login() {
     const [showPswd, setShowPswd] = useState(false)
     function handleSubmit(e){
         e.preventDefault()
-        // console.log(JSON.stringify(formValues))
-        console.log(formValues)
+        console.log(JSON.stringify(formValues))
+        const username = formValues.username
+        const password = formValues.password
+        const users = {
+            username: "tony",
+            password: "test",
+        }
+        if (username !== users.username || password !== users.password) {
+            console.log("User not found")
+            return
+        }  
+        else {
+            console.log(`Welcome back ${username}`)
+        }
         setFormValues({
             username: "",
             password: "",
         })
+        e.preventDefault()
     }
 return (
     <section className='loginInfo'>
