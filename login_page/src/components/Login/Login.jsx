61e0e75027a284b366ef4492bb62b49980f4c8cc
Anthony <Anthony.Torrefranca@gmail.com>
2026-06-23 14:16:15 +0800

    feat: transfer the show password state to function for readable snippet

--- login_page/src/components/Login/Login.jsx
+++ login_page/src/components/Login/Login.jsx
@@ -2,71 +2,76 @@ import { useState } from "react"
 import './Login.css'
 
 export default function Login() {
-    const [ShowPswd, setShowPswd] = useState(false)
-    const [formValues, setFormValues] = useState({
+    const [ShowPswd, setShowPswd] = useState(false) 
+    const [FormValues, setFormValues] = useState({
         username: "",
         password: "",
     })
     const userDetails = [
-        { username: "tony", password: "test" },
         { username: "simon", password: "test" },
-        { username: "john", password: "test" },
+        { username: "tony", password: "test" },
         { username: "tian", password: "test" },
-        { username: "david", password: "test" },
+        { username: "john", password: "test" },
     ]
+    const userFound = userDetails.find((user) => {
+        return user.username === FormValues.username && user.password === FormValues.password;
+    })
+
     function handleChange(e){
-        const { name, value } = e.target;
-        setFormValues(prev =>({
+        const {name, value} = e.target;
+        setFormValues(prev => ({
             ...prev,
-            [name]: value
+            [name]: value,
         }))
     }
     function handleSubmit(e){
-        e.preventDefault();
-        const userInput = formValues.username
-        const passInput = formValues.password
-        
-        if (userInput === "" || passInput === ""){
-        alert("Please enter credentials!")
+        e.preventDefault()
+        const username = FormValues.username === "";
+        const password = FormValues.password === "";
+        if (username || password){
+            alert("Please provide details!")
             return
         }
-        const userInfo = userDetails.find((user) =>{
-            return user.username === formValues.username && user.password === formValues.password
-        })
-        if (!userInfo){
-            alert("No account found!")
+        else if (!userFound){
+            alert("User not found!")
             return
         }
-        alert(`Welcome back, ${userInput}`)
-        }
-  return (
-    <section className="loginInfo">
-      <h1 className="welcomeHeading">Welcome back!</h1>
-      <p>login to continue your shopping journey.</p>
-      <form onSubmit={handleSubmit}>
-        <p>Enter username</p>
-        <section>
-            <input type="text" 
-                name="username"
-                value={formValues.username}
-                onChange={handleChange}
-                placeholder="Enter username" />                
-        </section>
-        <p>Enter password</p>
-        <section className="passwordContainer">
-            <input type={ShowPswd ? "text" : "password"} 
-                name="password"
-                value={formValues.password}
-                onChange={handleChange}
-                placeholder="Enter password" />                
-            <button type="button" onClick={()=> setShowPswd(!ShowPswd)}>👁</button>
-        </section>
-        <button type="submit" className="loginBtn">Login</button>
-      </form>
-      <section className="social">
-        <button className="continueWithSocials">Continue with Google</button>
-        <button className="continueWithSocials">Continue with Facebook</button>
-      </section>
+        alert(`Welcome back, ${FormValues.username}`)
+    }
+    function showPass(){
+        setShowPswd(!ShowPswd)
+    }
+return (
+<>
+    <section className="loginInfoContainer">
+        <h1 className="welcomHeading">Welcome Back!</h1>
+        <p>login to continue with your shopping journey.</p>
+        <form onSubmit={handleSubmit} className="loginInfo">
+            <p>Enter username</p>
+                <section>
+                    <input type="text"
+                        placeholder="Enter username" 
+                        name="username"
+                        value={FormValues.username}
+                        onChange={handleChange} />
+                </section>
+            <p>Enter username</p>
+                <section className="passwordContainer">
+                    <input type={ShowPswd ? "text" : "password"}
+                        placeholder="Enter password" 
+                        name="password"
+                        value={FormValues.password}
+                        onChange={handleChange} />
+                    <button type="button" onClick={showPass}>👁️</button>
+                </section>
+                    <button className="loginBtn" type="submit">Login</button>
+
+            <seciton className="social">
+                <button className="continueWithSocials">Continue with Google</button>
+                <button className="continueWithSocials">Continue with Facebook</button>
+            </seciton>
+        </form>
     </section>
+</>
   )
 }
