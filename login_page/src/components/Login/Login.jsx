c044e77e5e7a0bb7dcd4ef3d9942239ef0213229
Anthony <Anthony.Torrefranca@gmail.com>
2026-06-24 18:32:44 +0800

    feat: added two useEffect

--- login_page/src/components/Login/Login.jsx
+++ login_page/src/components/Login/Login.jsx
@@ -1,107 +1,113 @@
 import { useState, useEffect } from 'react'
 import './Login.css'
-import App from '../../App';
 
 export default function Login() {
-    const [ShowPswd, setShowPswd] = useState(false)
+    //! mock data base
+    const userDetails = ([
+        {username: "tony", password: "test"},
+        {username: "simon", password: "test"},
+        {username: "david", password: "test"},
+        {username: "tian", password: "test"},
+    ])
+    const [FormStatus, setFormStatus] = useState({
+        userFound: false,
+        userNotFound: false,
+        showPswd: false,
+    })
     const [FormValues, setFormValues] = useState({
         username: "",
         password: "",
     })
-    const [Appear, setAppear] = useState({
-        ifUserFound: false,
-        emptyUsername: false,
-        emptyPassword: false,
-    })
-    const userDetails = ([
-        {username: "simon", password:"test"},
-        {username: "tony", password:"test"},
-        {username: "tian", password:"test"},
-        {username: "john", password:"test"},
-    ])
-    const userCheck = userDetails.find((user) =>{
+    const userCheck = userDetails.find(user =>{
         return user.username === FormValues.username && user.password === FormValues.password;
     })
-    useEffect(()=> { 
-        if(Appear){ 
-            const timer = setTimeout(()=>
-                setAppear({ 
-                    ifUserFound: false, 
-                    emptyUsername: false, 
-                    emptyPassword: false,
-                }), 2000)
-                return ()=> clearTimeout(timer);
-        }},[Appear])
-        function handleChange(e){
-            const { name, value } = e.target
-            setFormValues(prev => ({
+
+    useEffect(()=>{
+        let timer;
+        if (FormStatus.userNotFound){
+            timer = setTimeout(() => {
+                setFormStatus(prev =>({
+                    ...prev,
+                    userFound: false,
+                    userNotFound: false,
+                }))
+            }, 2000);
+        }
+        
+        else if (FormStatus.userFound){
+            timer = setTimeout(() => {
+                setFormStatus(prev =>({
+                    ...prev,
+                    userFound: false,
+                    userNotFound: false,
+                }))
+            }, 2000);
+    return () => clearTimeout(timer)
+}}, [FormStatus.userNotFound, FormStatus.userFound])
+    
+    function handleShow(){
+        setFormStatus(prev =>({
+            ...prev,
+            showPswd: !prev.showPswd,
+        }))
+    }
+    function handleChange(e){
+        const {name, value} = e.target;
+        setFormValues(prev => ({
+            ...prev,
+            [name]: value,
+        }))
+    }
+    function handleSubmit(e){
+        e.preventDefault()
+        if (!userCheck){
+            setFormStatus(prev => ({
                 ...prev,
-                [name]: value,
+                userFound: false,
+                userNotFound: true,
             }))
         }
-        function handleSubmit(e){
-            e.preventDefault()
-            const username = FormValues.username === "";
-            const password = FormValues.password === "";
-            
-            if (username){
-                setAppear(
-                    (prev) => ({ ...prev, 
-                    emptyUsername: true}
-                ))
-                return
-            }
-        else if (password){
-            setAppear(
-                (prev) => ({ ...prev, 
-                emptyPassword: true}
-            ))
-            return
+        else if (userCheck){
+            setFormStatus( (prev) => ({
+                ...prev,
+                userFound: true,
+                userNotFound: false,
+            }))
         }
-        else if(!userCheck){
-            setAppear(
-                (prev) => ({ ...prev,
-                    ifUserFound: true
-                })
-            )
-            return
-        }
-
-        setFormValues({
-            username: "",
-            password: "",
-        })
-    }
-    function showPassword(){
-        setShowPswd(!ShowPswd)
     }
   return (
-    <section className="loginInfoContainer">
-        <form onSubmit={handleSubmit} className="loginInfo">
-            <h1 className="welcomeHeading">Welcome Back!</h1>
-            <p>login to continue your shopping journey.</p>
-            { Appear.ifUserFound && <h2 className='errorKey'>User not found!</h2>}
-            { Appear.emptyUsername && <h2 className='errorKey'>Please provide Username!</h2>}
-            { Appear.emptyPassword && <h2 className='errorKey'>Please provide Password!</h2>}
-            <p>Enter Username</p>
-                <section>
-                    <input type="text"
-                        name='username'
-                        value={FormValues.username}
-                        onChange={handleChange}
-                        placeholder="Enter Username" />
-                </section>
-            <p>Enter Password</p>
-                <section className="passwordContainer">
-                    <input type={ShowPswd? "text" : "password"}
-                        name='password'
-                        value={FormValues.password}
-                        onChange={handleChange}
-                        placeholder="Enter Password" />
-                    <button onClick={showPassword} type='button'>👁️</button>
-                </section>
-            <button type='submit' className='loginBtn'>Login</button>
-        </form> 
+    <>
+    <section className='loginInfoContainer'>
+        <h1 className="welccomHeading">Welcome Back!</h1>
+        <p>Login to continue your shopping journey.</p>
+    <form onSubmit={handleSubmit} className='loginInfo'>
+    {FormStatus.userNotFound && <h2 className='errorKey'>User not found!</h2>}
+    {FormStatus.userFound && <h2 className='correctKey'>Welcome Back, {FormValues.username}!</h2>}
+        <p>Enter Username</p>
+        <section>
+            <input type="text"
+                name='username'
+                value={FormValues.username}
+                onChange={handleChange}
+                placeholder='Enter username' />
+        </section>
+        <p>Enter Password</p>
+        <section className='passwordContainer'>
+            <input type={FormStatus.showPswd ? "text" : "password"}
+                name='password'
+                value={FormValues.password}
+                onChange={handleChange}
+                placeholder='Enter password' />
+            <button type='button' onClick={handleShow}>👁️</button>
+        </section>
+            <button className='loginBtn'>Login</button>
+        <p>or</p>
+        <section className="social">
+            <button className='continueWithSocials'>Continue with Google</button>
+            <button className='continueWithSocials'>Continue with Facebook</button>
+        </section>
+    </form>
     </section>
+    </>
   )
 }
