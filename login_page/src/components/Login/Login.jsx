3a6c579496b1ccaad5c7b830ad4e497a5bde7405
Anthony <Anthony.Torrefranca@gmail.com>
2026-06-26 16:33:25 +0800

    feat: implement client-side login authentication from memory

--- login_page/src/components/Login/Login.jsx
+++ login_page/src/components/Login/Login.jsx
@@ -1,107 +1,109 @@
-import { useEffect, useState }from 'react'
+import { useEffect, useState } from "react"
 import './Login.css'
 
 export default function Login() {
-    const userDetails = [
-        { username: "tony", password: "test"},
-        { username: "simon", password: "test"},
+    const userDetails = ([
         { username: "tian", password: "test"},
-        { username: "david", password: "test"},
-    ]
-    const [ShowPswd, setShowPswd] = useState(false)
+        { username: "john", password: "test"},
+        { username: "simon", password: "test"},
+        { username: "tony", password: "test"},
+    ])
     const [FormValues, setFormValues] = useState({
         username: "",
-        password: ""
+        password: ""    
     })
     const [UserStatus, setUserStatus] = useState({
         userFound: false,
         userNotFound: false,
     })
+    const [show, setShow] = useState(false)
     
-    useEffect(() => {
+    useEffect(() =>{
         let timer;
         if (UserStatus.userFound){
             timer = setTimeout(() => {
-                setUserStatus(prev =>({
+                setUserStatus(prev => ({
                     ...prev,
-                    userFound: false,
-                })) 
+                    userFound: false
+                }))
             }, 2000);
         } 
         else if (UserStatus.userNotFound){
             timer = setTimeout(() => {
                 setUserStatus(prev => ({
-                    ...prev, 
+                    ...prev,
                     userNotFound: false
-                }))    
-            }, 2000); 
+                }))
+            }, 2000);
         }
         return ()=> clearTimeout(timer)
     }, [UserStatus.userFound, UserStatus.userNotFound])
 
     function handleShow(){
-        setShowPswd(!ShowPswd)
+        setShow(!show)
     }
     function handleChange(e){
         const {name, value} = e.target
-        setFormValues((prev) => ({
+        setFormValues(prev => ({
             ...prev,
-            [name]: value,
+            [name]: value
         }))
     }
     function handleSubmit(e){
         e.preventDefault()
-        const userCheck = userDetails.find((user) => {
+        const userCheck = userDetails.find((user) =>{
             return user.username === FormValues.username && user.password === FormValues.password;
-            }
-        )
+        })
         if (userCheck){
-            setUserStatus((prev) => ({
+            setUserStatus(prev => ({
                 ...prev,
                 userFound: true,
-                userNotFound: false
+                userNotFound:false
             }))
         }
         else if (!userCheck){
-            setUserStatus(prev => ({
+            setUserStatus(prev =>({
                 ...prev,
                 userFound: false,
                 userNotFound: true
             }))
         }
     }
+    
   return (
-    <section className='loginInfoContainer'>
-        <section className="loginInfo">
-            <h1 className="welcomeHeading">Welcome Back!</h1>
-            {UserStatus.userFound && <h3 className='correctKey'>User found!!</h3>}
-            {UserStatus.userNotFound && <h3 className='errorKey'>User not found!!</h3>}
-            <p>login to continue your shopping journey.</p>
-            <form onSubmit={handleSubmit}>
-                <p>Enter username</p>
-                    <section>
-                        <input type="text"
-                            placeholder='Enter Password'
-                            name='username'
-                            value={FormValues.username}
-                            onChange={handleChange} /> 
-                    </section>
-                <p>Enter password</p>
-                    <section className='passwordContainer'>
-                        <input type={ShowPswd ? "text" : "password"}
-                            placeholder='Enter Password'
-                            name='password'
-                            value={FormValues.password}
-                            onChange={handleChange} />
-                        <button type='button' onClick={handleShow}>👁️</button>
-                    </section>
-                <button type='submit' className='loginBtn'>Login</button>
-            </form>
-            <section className="social">
-                <button className='continueWithSocials'>Continue with Google</button>
-                <button className='continueWithSocials'>Continue with Facebook</button>
+    <section className="loginInfoContainer">
+      <section className="loginInfo">
+        <h1 className="welcomeHeading">Welcome Back!</h1>
+        <p>login to continue your shopping journey.</p>
+        {UserStatus.userFound && <h2 className="correctKey">Welcome back, {FormValues.username}</h2>}
+        {UserStatus.userNotFound && <h2 className="incorrectKey">Enter correct credentials!</h2>}
+        <form onSubmit={handleSubmit}>
+            <p>Enter Username</p>
+            <section>
+                <input type="text"
+                    name="username"
+                    placeholder="Enter Username"
+                    value={FormValues.username}
+                    onChange={handleChange}
+                     />
             </section>
-        </section>
+            <p>Enter Password</p>
+            <section className="passwordContainer">
+                <input type={show ? "text" : "password"}
+                    name="password"
+                    placeholder="Enter Password"
+                    value={FormValues.password}
+                    onChange={handleChange}
+                     />
+                <button type="button" onClick={handleShow}>👁️</button>
+            </section>
+            <button type="submit" className="loginBtn">Login</button>
+        </form>
+      </section>
+      <section className="social">
+        <button className="continueWithSocials">Continue with Google</button>
+        <button className="continueWithSocials">Continue with Facebook</button>
+      </section>
     </section>
   )
 }
